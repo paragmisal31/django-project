90ac658fb3fca253f455e543eb63ea148c1e4437
paragmisal31 <44521963+paragmisal31@users.noreply.github.com>
2020-10-01 11:21:52 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+
+#STATICFILES_DIRS = [
+#os.path.join(BASE_DIR, "static","static_file"),
+#]
+
+#STATICFILES_FINDERS = (
+ #   'django.contrib.staticfiles.finders.FileSystemFinder',
+ #   'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+    )
+
+#MEDIA_URL ='/media/'
+#MEDIA_ROOT =os.path.join(BASE_DIR, "media_root")
